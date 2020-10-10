327dec88c6d0ba31fbd3df3fbe34c93af817b082
Atef Ben Ali <atef.bettaib@gmail.com>
2020-10-10 18:55:03 +0100

    refactor: review `getting data form api` logic in `app` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
-import fetch from 'cross-fetch';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import fetch from "cross-fetch";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  fetch(`http://${process.env.HOST}:${process.env.PORT}/api`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [apiMessage, setApiMessage] = React.useState<string>("");
 
+  React.useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      let apiReq = await fetch(
+        `http://${process.env.HOST}:${process.env.PORT}/api`
+      );
+
+      apiReq
+        .json()
+        .then(data => {
+          setIsLoading(false);
+          let apiRes = JSON.parse(data);
+          if (apiRes.hasOwnProperty("message")) {
+            setApiMessage(apiRes.message);
+          }
+        })
+        .catch(e => console.log(`Error: ${e.message}.`));
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+  if (apiMessage.length) {
+    return <p>Api message: {apiMessage}</p>;
+  }
   return (
     <Switch>
       <Route exact={true} path="/" component={Home} />
